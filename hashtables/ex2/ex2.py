84a6a9523f78bcc7d6a83c55d4d404dc80c3780c
Colin de Vries <colindevries325@gmail.com>
2020-02-14 10:53:54 -0800

    add comments to hashtable ex2 problem

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,21 +16,29 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
+    # INSERT ALL TICKETS INTO HASHTABLE
     for item in tickets:
         hash_table_insert(hashtable, item.source, item.destination)
 
+    # VALUE OF FIRST LOCATION IS NONE, SET LOCATION TO FIRST VALUE IN ROUTE ARRAY
     first = hash_table_retrieve(hashtable, 'NONE')
-
     route[0] = first
 
+    # SET CURRENT INDEX TO 1, STARTING AT 2ND ITEM
     current_index = 1
-    while current_index < length - 1:
+
+    # WHILE CURRENT INDEX IS LESS THAN LENGTH OF ARRAY, USE PREVIOUS LOCATION AS PARAMETER IN HASHTABLE RETRIEVE
+    for i in range(length):
         if hash_table_retrieve(hashtable, route[current_index - 1]) != 'NONE':
             next_location = hash_table_retrieve(
                 hashtable, route[current_index - 1])
+
+            # SET THE LOCATION TO BE THE NEXT ITEM IN ROUTE ARRAY
             route[current_index] = next_location
+            current_index += 1
         else:
+
+            # IF LOCATION VALUE IS NONE, SET THAT LOCATION TO BE THE LAST IN THE ARRAY
             route[length - 1] = route[current_index - 1]
-        current_index += 1
 
     return route[:length - 1]
