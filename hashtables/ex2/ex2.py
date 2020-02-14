5293ee3a1f75dd8ac2117eb09db5025557a7cff1
Colin de Vries <colindevries325@gmail.com>
2020-02-14 10:47:13 -0800

    completed ex2 hashtable problem

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,21 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for item in tickets:
+        hash_table_insert(hashtable, item.source, item.destination)
 
-    pass
+    first = hash_table_retrieve(hashtable, 'NONE')
+
+    route[0] = first
+
+    current_index = 1
+    while current_index < length - 1:
+        if hash_table_retrieve(hashtable, route[current_index - 1]) != 'NONE':
+            next_location = hash_table_retrieve(
+                hashtable, route[current_index - 1])
+            route[current_index] = next_location
+        else:
+            route[length - 1] = route[current_index - 1]
+        current_index += 1
+
+    return route[:length - 1]
